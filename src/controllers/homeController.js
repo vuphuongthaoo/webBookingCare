5fcf328cb6f91e2ae0463c8156c50ac3084d5bbb
VuThao <vuthaothao412@gmail.com>
2022-03-10 23:54:06 +0000

    update-two

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,27 +1,33 @@
-import db from '../models/index';
-
+import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
-
-    try{
-        let data = await db.User.findAll();
-        return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
-        });
-    }catch (e) {
-        console.log(e)
-    }
-      
-}
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-}
+  return res.render("test/about.ejs");
+};
 
-// object: {
-//     key: '',
-//     value: ''
-// }
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
+
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-}
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+};

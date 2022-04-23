c8dda8a005063e472253f76653e22871bb6657ee
VuThao <vuthaothao412@gmail.com>
2022-04-23 16:27:33 +0700

    update-thirteen

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -173,17 +173,10 @@ let bulkCreateSchedule = (data) => {
           attributes: ["timeType", "date", "doctorId", "maxNumber"],
           raw: true,
         });
-        //convenrt date
-        if (existing && existing.length > 0) {
-          existing = existing.map((item) => {
-            item.date = new Date(item.date).getTime();
-            return item;
-          });
-        }
 
         //compare different
         let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-          return a.timeType === b.timeType && a.date === b.date;
+          return a.timeType === b.timeType && +a.date === +b.date;
         });
         //create data
         if (toCreate && toCreate.length > 0) {
@@ -214,6 +207,15 @@ let getScheduleByDate = (doctorId, date) => {
             doctorId: doctorId,
             date: date,
           },
+          include: [
+            {
+              model: db.Allcode,
+              as: "timeTypeData",
+              attributes: ["valueEn", "valueVi"],
+            },
+          ],
+          raw: false,
+          nest: true,
         });
 
         if (!dataSchedule) dataSchedule = [];

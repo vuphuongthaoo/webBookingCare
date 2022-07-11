8d03fa8e6be5229c2a7dbcb4c8775eea9014049d
VuThao <vuthaothao412@gmail.com>
2022-07-12 00:28:24 +0700

    update-twenty-two

--- src/migrations/migration-create-specialty.js
+++ src/migrations/migration-create-specialty.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('specialties', {
-        id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER
-        },
-        name: {
-            type: Sequelize.STRING
-        },
-        description: {
-            type: Sequelize.TEXT
-        },
-        image: {
-            type: Sequelize.STRING
-        },
-        createdAt: {
-            allowNull: false,
-            type: Sequelize.DATE
-        },
-        updatedAt: {
-            allowNull: false,
-            type: Sequelize.DATE
-        }
+    await queryInterface.createTable("specialties", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        type: Sequelize.BLOB("long"),
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      descriptionHTML: {
+        type: Sequelize.TEXT,
+      },
+      descriptionMarkdorn: {
+        type: Sequelize.TEXT,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('specialties');
-  }
+    await queryInterface.dropTable("specialties");
+  },
 };
--- src/models/specialty.js
+++ src/models/specialty.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Specialty extends Model {
     /**
@@ -12,14 +10,18 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Specialty.init({
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    image: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Specialty',
-  });
+  }
+  Specialty.init(
+    {
+      name: DataTypes.STRING,
+      descriptionMarkdorn: DataTypes.TEXT,
+      descriptionHTML: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Specialty",
+    }
+  );
   return Specialty;
 };
